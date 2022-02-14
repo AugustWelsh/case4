064253dd22142edebb74911f2d873851e3e42a9b
AugustWelsh <90603561+AugustWelsh@users.noreply.github.com>
2022-02-14 08:32:04 -0500

    Add files via upload

--- api2.js
+++ api2.js
@@ -4,6 +4,7 @@ const inputElement = document.getElementById('areaCode')
 const result = document.getElementById('result')
 
 
+
 function fetchUserData() {
     fetch('api.json')
         .then(response => response.json())
@@ -18,21 +19,43 @@ function fetchUserData() {
 function render(obj) {
     obj.data.forEach(element => {
         //console.log(element.restaurant_name)
-        result.innerHTML += `<li>${element.restaurant_name}</li>`
+        result.innerHTML += `<li><a href="#">${element.restaurant_name}</a><a href="#"><p>${element.cuisines}</p></a></li>`
        // result.innerHTML += `<h4>${element.cuisines}</h4>`
        // result.innerHTML += `<h4>${element.restaurant_phone}</h4>`
         
     })
 
 }
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("result");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
 
 function filter(obj) {
 
     //this is how to make a filter
     //let data = obj.data.filter()
-   
+  
+
     return obj;
 }
+
+
 
 
 
