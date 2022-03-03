d3d2138c6d6221207a559ada082c5c91675fc23f
AugustWelsh <90603561+AugustWelsh@users.noreply.github.com>
2022-03-03 05:15:26 -0500

    Add files via upload

--- api2.js
+++ api2.js
@@ -1,11 +1,12 @@
-const searchElement = document.getElementById('myInput').addEventListener('keyup', myInput);
+//const searchElement = document.getElementById('myInput').addEventListener('keyup', myInput);
+const searchElement = document.getElementById('fetchUserDataBtn').addEventListener('click', fetchUserData);
 const optionElement = document.getElementById('option')
 const inputElement = document.getElementById('areaCode')
 const result = document.getElementById('result')
 
 
 
-function myInput() {
+function /*myInput*/fetchUserData() {
     fetch('api.json')
         .then(response => response.json())
         .then(obj => {
@@ -19,7 +20,7 @@ function myInput() {
 function render(obj) {
     obj.data.forEach(element => {
         //console.log(element.restaurant_name)
-        result.innerHTML += `<li><a href="#">${element.restaurant_name}</a><a href="#"><p>${element.cuisines}</p></a></li>`
+        result.innerHTML += `<li><a>${element.restaurant_name} <br> <p>Cuisines:</p><h4>${element.cuisines}</h4> <h4>${element.restaurant_website}</h4> </a></li>`
        // result.innerHTML += `<h4>${element.cuisines}</h4>`
        // result.innerHTML += `<h4>${element.restaurant_phone}</h4>`
         
@@ -28,11 +29,12 @@ function render(obj) {
 }
 function myFunction() {
     // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, ul, li, a, i, p, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("result");
     li = ul.getElementsByTagName('li');
+    p = ul.getElementsByTagName('p')
   
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
